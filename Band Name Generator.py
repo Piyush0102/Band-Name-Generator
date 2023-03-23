fd17b6d38c822b31b843d4188eea8cee01400694
Piyush Kale <91786505+Piyush0102@users.noreply.github.com>
2023-03-23 15:18:00 +0530

    Add files via upload

---
+++ Band Name Generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("Welcome to the Band Name Generator.")
+city_name=input("What's the name of the city you grew up in?\n")
+pet_name=input("What's the name of your pet?\n")
+print("Your Band Name could be "+city_name+" "+pet_name)
+
+
+# In[ ]:
+
+
+
+
